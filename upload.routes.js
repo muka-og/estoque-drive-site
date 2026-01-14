650e25c9e7042686884d40f2b31034a263a828b6
muka-og <bhsamuel4@gmail.com>
2026-01-14 09:52:38 -0300

    Delete upload.routes.js

--- upload.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { upload } from "../middlewares/upload.js";
-import { uploadFotos } from "../controllers/uploadController.js";
-
-const router = Router();
-router.post("/", upload.array("fotos"), uploadFotos);
-
-export default router;
