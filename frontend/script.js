6683f3b6ab410f086f8cf80f736b229d46f492f7
muka-og <bhsamuel4@gmail.com>
2026-01-14 13:25:38 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,37 @@
 const form = document.getElementById("formUpload");
 const status = document.getElementById("status");
 
+// Coloque aqui a URL pública do seu backend
+const API_URL = "https://railway.com/project/cba879f1-eaf7-4607-8d5a-f21a34b15494?environmentId=f0ac7dc9-6ad0-4152-963d-238e8946ace2"; // substitua pela sua URL
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  status.innerText = "Enviando pedido...";
+  status.textContent = "Enviando pedido...";
 
   const formData = new FormData(form);
 
   try {
-    const response = await fetch("http://localhost:3333/upload", {
+    const response = await fetch(`${API_URL}/pedido`, {
       method: "POST",
       body: formData
     });
 
     if (!response.ok) {
-      const text = await response.text();
-      throw new Error(text);
+      throw new Error(`Erro do servidor: ${response.status}`);
     }
 
     const data = await response.json();
-    console.log("Resposta API:", data);
 
-    status.innerText = `Pedido enviado! Código: ${data.codigo}`;
+    if (data.success) {
+      status.textContent = `Pedido enviado! Código: ${data.codigoPedido}`;
+      form.reset();
+    } else {
+      status.textContent = "Falha ao enviar pedido.";
+    }
 
   } catch (error) {
-    console.error("ERRO REAL:", error);
-    status.innerText = "❌ Erro: " + error.message;
+    console.error(error);
+    status.textContent = "Erro ao conectar com o servidor.";
   }
 });
