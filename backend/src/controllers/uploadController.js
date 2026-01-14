e2d90139c3bc9ffe6ced1375206338b0ed6e571b
muka-og <bhsamuel4@gmail.com>
2026-01-14 09:30:49 -0300

    Implement uploadFotos function in uploadController

---
+++ backend/src/controllers/uploadController.js
@@ -0,0 +1,17 @@
+import { salvarNoDrive } from "../services/driveService.js";
+import { formatPlaca } from "../utils/formatPlaca.js";
+import { dataHoje } from "../utils/formatData.js";
+
+export async function uploadFotos(req, res) {
+  try {
+    const placa = formatPlaca(req.body.placa);
+    const data = dataHoje();
+
+    await salvarNoDrive(req.files, data, placa);
+
+    res.json({ sucesso: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: error.message });
+  }
+}
